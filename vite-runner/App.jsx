6bd67041d08f8252a9545a216a27bb05f955a5e3
乔国胜 <294238921@qq.com>
2024-02-06 14:06:58 +0800

    自定义react名字

--- vite-runner/App.jsx
+++ vite-runner/App.jsx
@@ -1,12 +1,16 @@
-import React from "./core/React.js";
+
+/** @jsx CReact.createElement */
+import CReact from "./core/React.js";
 // const App = React.createElement("div", { id: "app" }, "hi, min-react");
 
 function App1() {
     return <div>hello, min-react</div>
 }
 
+console.log('App1', App1)
+
 const App = <div id='main'>hello, min-react <p>bridge</p></div>;
 
-console.log('App1', App1)
+
 
 export default App;
